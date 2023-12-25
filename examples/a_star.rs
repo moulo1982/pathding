4990d0bda354755064bea9da4a1f47520cc87d27
moulo <moulo@126.com>
2023-12-25 15:37:17 +0800

    所以编写一些测试用例

--- examples/a_star.rs
+++ examples/a_star.rs
@@ -22,17 +22,17 @@ async fn main() {
         return;
     }
 
-    let threads: Vec<_> = (0..5)
-        .map(|i| {
+    let threads: Vec<_> = (0..1_000)
+        .map(|_i| {
             let map = map.clone();
             tokio::spawn(async move {
-                let result = map.read().await.find_path(mm, &map::Point::new(1, 0), &map::Point::new(6, 7));
-                println!("{}, {:?}", i, result);
+                let _result = map.read().await.find_path(mm, &map::Point::new(1, 0), &map::Point::new(6, 7));
+                //println!("{}, {:?}", i, result);
             })
         })
         .collect();
 
-
+    println!("Total: {}", threads.len());
     for thread in threads {
         thread.await.unwrap()
     }
--- tests/benchmark.rs
+++ tests/benchmark.rs
@@ -1,1 +1,39 @@
+use std::sync::Arc;
+use game_pathfinding::map::MAP_MANAGER;
+use game_pathfinding::{map, vec2d};
 
+#[tokio::test]
+async fn many_test() {
+    let map_info = vec2d![
+        1, 0, 1, 0, 1, 0, 1, 0;
+        0, 0, 0, 1, 0, 1, 0, 1;
+        1, 0, 1, 0, 1, 0, 1, 0;
+        0, 0, 0, 1, 0, 1, 0, 1;
+        1, 0, 1, 0, 1, 0, 1, 0;
+        0, 0, 0, 1, 0, 1, 0, 1;
+        1, 0, 1, 0, 1, 0, 1, 0;
+        0, 0, 0, 1, 0, 1, 0, 1;
+    ];
+
+    let map = Arc::clone(&MAP_MANAGER);
+    let mm = map.write().await.new_astar().await;
+    if let Err(err) = map.write().await.load(mm, map_info) {
+        println!("{}", err);
+        return;
+    }
+
+    let threads: Vec<_> = (0..1_000_000)
+        .map(|_i| {
+            let map = map.clone();
+            tokio::spawn(async move {
+                let _result = map.read().await.find_path(mm, &map::Point::new(1, 0), &map::Point::new(6, 7));
+                //println!("{}, {:?}", i, result);
+            })
+        })
+        .collect();
+
+    println!("Total: {}", threads.len());
+    for thread in threads {
+        thread.await.unwrap()
+    }
+}
